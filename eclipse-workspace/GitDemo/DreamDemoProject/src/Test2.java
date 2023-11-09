48e76759b7d5cee118a30e1d8c6dcc5f90bd1a6b
ChiranjeeviHeggade <51851281+ChiranjeeviHeggade@users.noreply.github.com>
2023-11-08 18:21:48 -0600

    Update Test2.java
    New Statement Added

--- eclipse-workspace/GitDemo/DreamDemoProject/src/Test2.java
+++ eclipse-workspace/GitDemo/DreamDemoProject/src/Test2.java
@@ -4,8 +4,10 @@ public class Test2 {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello World");
-			
-
+		
+		// SAY HELLO TO USA	
+                System.out.println("Hello USA");
+		
 	}
 
 }
